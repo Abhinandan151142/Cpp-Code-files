0502d98912c70b3af82db83614937297ce84dd30
Abhinandan151142 <abhinandangupta039@gmail.com>
2025-08-12 17:24:19 +0530

    removing commented code

--- code1.cpp
+++ code1.cpp
@@ -5,7 +5,7 @@ int main() {
     cin >> n >> m;
     int a[n][m];
     
-    // Input the matrix
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
